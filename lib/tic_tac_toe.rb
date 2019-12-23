46b177b2b1cc7237fb6936b6b6b8c686feb18770
Asha Fernandez <ash.fernan90@gmail.com>
2019-12-23 04:24:52 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -54,7 +54,7 @@ class TicTacToe
   end
 
   def valid_move?(input)
-    input.to_i.between?(0, 8) && !position_taken?(input.to_i-1)
+    input.to_i.between?(1, 9) && !position_taken?(input.to_i-1)
   end
 
   def current_player
