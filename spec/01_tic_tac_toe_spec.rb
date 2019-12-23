38d47be846c402f2951bb842ca4b9f42ab0c193e
Asha Fernandez <ash.fernan90@gmail.com>
2019-12-23 04:19:31 +0000

    Automatically backed up by Learn

--- spec/01_tic_tac_toe_spec.rb
+++ spec/01_tic_tac_toe_spec.rb
@@ -9,6 +9,21 @@ describe './lib/tic_tac_toe.rb' do
       end
     end
 
+    describe 'WIN_COMBINATIONS' do
+      it 'defines a constant WIN_COMBINATIONS with arrays for each win combination' do
+        expect(TicTacToe::WIN_COMBINATIONS.size).to eq(8)
+
+        expect(TicTacToe::WIN_COMBINATIONS).to include_array([0,1,2])
+        expect(TicTacToe::WIN_COMBINATIONS).to include_array([3,4,5])
+        expect(TicTacToe::WIN_COMBINATIONS).to include_array([6,7,8])
+        expect(TicTacToe::WIN_COMBINATIONS).to include_array([0,3,6])
+        expect(TicTacToe::WIN_COMBINATIONS).to include_array([1,4,7])
+        expect(TicTacToe::WIN_COMBINATIONS).to include_array([2,5,8])
+        expect(TicTacToe::WIN_COMBINATIONS).to include_array([0,4,8])
+        expect(TicTacToe::WIN_COMBINATIONS).to include_array([6,4,2])
+      end
+    end
+
     describe '#display_board' do
       it 'prints arbitrary arrangements of the board' do
         board = ["X", "X", "X", "X", "O", "O", "X", "O", "O"]
@@ -27,233 +42,268 @@ describe './lib/tic_tac_toe.rb' do
         board = ["X", "O", "X", "O", "X", "X", "O", "X", "O"]
         game.instance_variable_set(:@board, board)
 
-        output = capture_puts{ game.display_board }
+    require_relative '../lib/tic_tac_toe.rb'
 
-        expect(output).to include(" X | O | X ")
-        expect(output).to include("-----------")
-        expect(output).to include(" O | X | X ")
-        expect(output).to include("-----------")
-        expect(output).to include(" O | X | O ")
-      end
+describe './lib/tic_tac_toe.rb' do
+  describe 'WIN_COMBINATIONS' do
+    it 'defines a constant WIN_COMBINATIONS with arrays for each win combination' do
+      expect(WIN_COMBINATIONS.size).to eq(8)
+
+      expect(WIN_COMBINATIONS).to include_array([0,1,2])
+      expect(WIN_COMBINATIONS).to include_array([3,4,5])
+      expect(WIN_COMBINATIONS).to include_array([6,7,8])
+      expect(WIN_COMBINATIONS).to include_array([0,3,6])
+      expect(WIN_COMBINATIONS).to include_array([1,4,7])
+      expect(WIN_COMBINATIONS).to include_array([2,5,8])
+      expect(WIN_COMBINATIONS).to include_array([0,4,8])
+      expect(WIN_COMBINATIONS).to include_array([6,4,2])
+    end
+  end
+
+  describe '#display_board' do
+    it 'prints arbitrary arrangements of the board' do
+      board = ["X", "X", "X", "X", "O", "O", "X", "O", "O"]
+
+      output = capture_puts{ display_board(board) }
+
+      expect(output).to include(" X | X | X ")
+      expect(output).to include("-----------")
+      expect(output).to include(" X | O | O ")
+      expect(output).to include("-----------")
+      expect(output).to include(" X | O | O ")
+
+
+      board = ["X", "O", "X", "O", "X", "X", "O", "X", "O"]
+
+      output = capture_puts{ display_board(board) }
+
+      expect(output).to include(" X | O | X ")
+      expect(output).to include("-----------")
+      expect(output).to include(" O | X | X ")
+      expect(output).to include("-----------")
+      expect(output).to include(" O | X | O ")
+    end
+  end
+
+  describe '#input_to_index' do
+
+    it 'converts user_input to an integer' do
+      user_input = "1"
+
+      expect(input_to_index(user_input)).to be_a(Integer)
     end
 
-    describe 'WIN_COMBINATIONS' do
-      it 'defines a constant WIN_COMBINATIONS with arrays for each win combination' do
-        expect(TicTacToe::WIN_COMBINATIONS.size).to eq(8)
+    it 'subtracts 1 from the user_input' do
+      user_input = "6"
 
-        expect(TicTacToe::WIN_COMBINATIONS).to include([0,1,2])
-        expect(TicTacToe::WIN_COMBINATIONS).to include([3,4,5])
-        expect(TicTacToe::WIN_COMBINATIONS).to include([6,7,8])
-        expect(TicTacToe::WIN_COMBINATIONS).to include([0,3,6])
-        expect(TicTacToe::WIN_COMBINATIONS).to include([1,4,7])
-        expect(TicTacToe::WIN_COMBINATIONS).to include([2,5,8])
-        expect(TicTacToe::WIN_COMBINATIONS).to include([0,4,8])
-        expect(TicTacToe::WIN_COMBINATIONS).to include([6,4,2])
-      end
+      expect(input_to_index(user_input)).to be(5)
     end
 
-    describe '#move' do
-      it 'allows "X" player in the top left and "O" in the middle' do
-        game = TicTacToe.new
-        game.move(1, "X")
-        game.move(5, "O")
+    it 'returns -1 for strings without integers' do
+      user_input = "invalid"
 
-        board = game.instance_variable_get(:@board)
-
-        expect(board).to eq(["X", " ", " ", " ", "O", " ", " ", " ", " "])
-      end
+      expect(input_to_index(user_input)).to be(-1)
     end
 
-    describe '#position_taken?' do
-      it 'returns true/false based on position in board' do
-        game = TicTacToe.new
-        board = ["X", " ", " ", " ", " ", " ", " ", " ", "O"]
-        game.instance_variable_set(:@board, board)
+  end
 
-        position = 0
-        expect(game.position_taken?(position)).to be(true)
+  describe '#move' do
 
-        position = 8
-        expect(game.position_taken?(position)).to be(true)
+    it 'does not allow for a default third argument' do
+      board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
-        position = 1
-        expect(game.position_taken?(position)).to be(false)
-
-        position = 7
-        expect(game.position_taken?(position)).to be(false)
-      end
+      expect{move(board, 0)}.to raise_error(ArgumentError)
     end
 
-    describe '#valid_move?' do
-      it 'returns true/false based on position' do
-        game = TicTacToe.new
-        board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
-        game.instance_variable_set(:@board, board)
+    it 'takes three arguments: board, position, and player token' do
+      board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
-        position = "1"
-        expect(game.valid_move?(position)).to be_truthy
-
-        position = "5"
-        expect(game.valid_move?(position)).to be_falsey
-
-        position = "invalid"
-        expect(game.valid_move?(position)).to be_falsey
-      end
+      expect{move(board, 0, "X")}.to_not raise_error
     end
 
-    describe '#turn' do
-      it 'makes valid moves' do
-        game = TicTacToe.new
-        allow($stdout).to receive(:puts)
+    it 'allows "X" player in the bottom right and "O" in the top left ' do
+      board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+      move(board, 0, "O")
+      move(board, 8, "X")
 
-        expect(game).to receive(:gets).and_return("1")
+      expect(board).to eq(["O", " ", " ", " ", " ", " ", " ", " ", "X"])
+    end
+  end
 
-        game.turn
-      end
+  describe '#position_taken?' do
+    it 'returns true/false based on position in board' do
+      board = ["X", " ", " ", " ", " ", " ", " ", " ", "O"]
 
-      it 'asks for input again after a failed validation' do
-        game = TicTacToe.new
-        allow($stdout).to receive(:puts)
+      position = 0
+      expect(position_taken?(board, position)).to be(true)
 
-        expect(game).to receive(:gets).and_return("invalid")
-        expect(game).to receive(:gets).and_return("1")
+      position = 8
+      expect(position_taken?(board, position)).to be(true)
 
-        game.turn
-      end
+      position = 1
+      expect(position_taken?(board, position)).to be(false)
+
+      position = 7
+      expect(position_taken?(board, position)).to be(false)
+    end
+  end
+
+  describe '#valid_move?' do
+    it 'returns true/false based on position' do
+      board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
+
+      position = 0
+      expect(valid_move?(board, position)).to be_truthy
+
+      position = 4
+      expect(valid_move?(board, position)).to be_falsey
+
+      position = -1
+      expect(valid_move?(board, position)).to be_falsey
+    end
+  end
+
+  describe '#turn' do
+    it 'makes valid moves' do
+      board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+      allow($stdout).to receive(:puts)
+
+      expect(self).to receive(:gets).and_return("1")
+
+      turn(board)
+
+      expect(board).to match_array(["X", " ", " ", " ", " ", " ", " ", " ", " "])
     end
 
-    describe '#turn_count' do
-      it 'counts occupied positions' do
-        game = TicTacToe.new
-        board = ["O", " ", " ", " ", "X", " ", " ", " ", "X"]
-        game.instance_variable_set(:@board, board)
+    it 'asks for input again after a failed validation' do
+      board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
-        expect(game.turn_count).to eq(3)
-      end
+      allow($stdout).to receive(:puts)
+
+      expect(self).to receive(:gets).and_return("invalid")
+      expect(self).to receive(:gets).and_return("1")
+
+      turn(board)
+    end
+  end
+
+  describe '#turn_count' do
+    it 'counts occupied positions' do
+      board = ["O", " ", " ", " ", "X", " ", " ", " ", "X"]
+
+      expect(turn_count(board)).to eq(3)
+    end
+  end
+
+  describe '#current_player' do
+    it 'returns the correct player, X, for the third move' do
+      board = ["O", " ", " ", " ", "X", " ", " ", " ", " "]
+
+      expect(current_player(board)).to eq("X")
+    end
+  end
+
+  describe "#won?" do
+    it 'returns false for a draw' do
+      board = ["X", "O", "X", "O", "X", "X", "O", "X", "O"]
+
+      expect(won?(board)).to be_falsey
     end
 
-    describe '#current_player' do
-      it 'returns the correct player, X, for the third move' do
-        game = TicTacToe.new
-        board = ["O", " ", " ", " ", "X", " ", " ", " ", " "]
-        game.instance_variable_set(:@board, board)
+    it 'returns true for a win' do
+      board = ["X", "O", "X", "O", "X", "X", "O", "O", "X"]
 
-        expect(game.current_player).to eq("X")
-      end
+      expect(won?(board)).to be_truthy
+    end
+  end
+
+  describe '#full?' do
+    it 'returns true for a draw' do
+      board = ["X", "O", "X", "O", "X", "X", "O", "X", "O"]
+
+      expect(full?(board)).to be_truthy
     end
 
-    describe "#won?" do
-      it 'returns false for a draw' do
-        game = TicTacToe.new
-        board = ["X", "O", "X", "O", "X", "X", "O", "X", "O"]
-        game.instance_variable_set(:@board, board)
+    it 'returns false for an in-progress game' do
+      board = ["X", " ", "X", " ", "X", " ", "O", "O", " "]
 
-        expect(game.won?).to be_falsey
-      end
+      expect(full?(board)).to be_falsey
+    end
+  end
 
-      it 'returns true for a win' do
-        game = TicTacToe.new
-        board = ["X", "O", "X", "O", "X", "X", "O", "O", "X"]
-        game.instance_variable_set(:@board, board)
+  describe '#draw?' do
 
-        expect(game.won?).to be_truthy
-      end
+    it 'calls won?' do
+      board = ["X", "O", "X", "O", "X", "X", "O", "X", "O"]
+
+      expect(self).to receive(:won?).with(board)
+      draw?(board)
     end
 
-    describe '#full?' do
-      it 'returns true for a draw' do
-        game = TicTacToe.new
-        board = ["X", "O", "X", "O", "X", "X", "O", "X", "O"]
-        game.instance_variable_set(:@board, board)
+    it 'calls full?' do 
+      board = ["X", "O", "X", "O", "X", "X", "O", "X", "O"]
 
-        expect(game.full?).to be_truthy
-      end
-
-      it 'returns false for an in-progress game' do
-        game = TicTacToe.new
-        board = ["X", " ", "X", " ", "X", " ", "O", "O", " "]
-        game.instance_variable_set(:@board, board)
-
-        expect(game.full?).to be_falsey
-      end
+      expect(self).to receive(:full?).with(board)      
+      draw?(board)
     end
 
-    describe '#draw?' do
-      it 'returns true for a draw' do
-        game = TicTacToe.new
-        board = ["X", "O", "X", "O", "X", "X", "O", "X", "O"]
-        game.instance_variable_set(:@board, board)
+    it 'returns true for a draw' do
+      board = ["X", "O", "X", "O", "X", "X", "O", "X", "O"]
 
-        expect(game.draw?).to be_truthy
-      end
-
-      it 'returns false for a won game' do
-        game = TicTacToe.new
-        board = ["X", "O", "X", "O", "X", "X", "O", "O", "X"]
-        game.instance_variable_set(:@board, board)
-
-        expect(game.draw?).to be_falsey
-      end
-
-      it 'returns false for an in-progress game' do
-        game = TicTacToe.new
-        board = ["X", " ", "X", " ", "X", " ", "O", "O", "X"]
-        game.instance_variable_set(:@board, board)
-
-        expect(game.draw?).to be_falsey
-      end
+      expect(draw?(board)).to be_truthy
     end
 
-    describe '#over?' do
-      it 'returns true for a draw' do
-        game = TicTacToe.new
-        board = ["X", "O", "X", "O", "X", "X", "O", "X", "O"]
-        game.instance_variable_set(:@board, board)
+    it 'returns false for a won game' do
+      board = ["X", "O", "X", "O", "X", "X", "O", "O", "X"]
 
-        expect(game.over?).to be_truthy
-      end
-
-      it 'returns true for a won game' do
-        game = TicTacToe.new
-        board = ["X", "O", "X", "O", "X", "X", "O", "O", "X"]
-        game.instance_variable_set(:@board, board)
-
-        expect(game.over?).to be_truthy
-      end
-
-      it 'returns false for an in-progress game' do
-        game = TicTacToe.new
-        board = ["X", " ", "X", " ", "X", " ", "O", "O", " "]
-        game.instance_variable_set(:@board, board)
-
-        expect(game.over?).to be_falsey
-      end
+      expect(draw?(board)).to be_falsey
     end
 
-    describe '#winner' do
-      it 'return X when X won' do
-        game = TicTacToe.new
-        board = ["X", " ", " ", " ", "X", " ", " ", " ", "X"]
-        game.instance_variable_set(:@board, board)
+    it 'returns false for an in-progress game' do
+      board = ["X", " ", "X", " ", "X", " ", "O", "O", "X"]
 
-        expect(game.winner).to eq("X")
-      end
+      expect(draw?(board)).to be_falsey
+    end
+  end
 
-      it 'returns O when O won' do
-        game = TicTacToe.new
-        board = ["X", "O", " ", " ", "O", " ", " ", "O", "X"]
-        game.instance_variable_set(:@board, board)
+  describe '#over?' do
+    it 'returns true for a draw' do
+      board = ["X", "O", "X", "O", "X", "X", "O", "X", "O"]
 
-        expect(game.winner).to eq("O")
-      end
+      expect(over?(board)).to be_truthy
+    end
 
-      it 'returns nil when no winner' do
-        game = TicTacToe.new
-        board = ["X", "O", " ", " ", " ", " ", " ", "O", "X"]
-        game.instance_variable_set(:@board, board)
+    it 'returns true for a won game' do
+      board = ["X", "O", "X", "O", "X", "X", "O", "O", "X"]
 
-        expect(game.winner).to be_nil
-      end
+      expect(over?(board)).to be_truthy
+    end
+
+    it 'returns false for an in-progress game' do
+      board = ["X", " ", "X", " ", "X", " ", "O", "O", " "]
+
+      expect(over?(board)).to be_falsey
+    end
+  end
+
+  describe '#winner' do
+    it 'return X when X won' do
+      board = ["X", " ", " ", " ", "X", " ", " ", " ", "X"]
+
+      expect(winner(board)).to eq("X")
+    end
+
+    it 'returns O when O won' do
+      board = ["X", "O", " ", " ", "O", " ", " ", "O", "X"]
+
+      expect(winner(board)).to eq("O")
+    end
+
+    it 'returns nil when no winner' do
+      board = ["X", "O", " ", " ", " ", " ", " ", "O", "X"]
+
+      expect(winner(board)).to be_nil
     end
   end
 end
